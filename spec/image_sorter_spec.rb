9fded8ee4d9ea6b58e1b96559f5072b5249bbd30
David Ashmon <david.ashmon@e-hps.com>
2024-04-15 00:13:55 -0500

    Linted using rubocop

--- spec/image_sorter_spec.rb
+++ spec/image_sorter_spec.rb
@@ -1,9 +1,10 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
+require 'spec_helper'
 require_relative '../image_sorter'
 
 payload =
-"photo.jpg, Krakow, 2013-09-05 14:08:15
+  "photo.jpg, Krakow, 2013-09-05 14:08:15
 Mike.png, London, 2015-06-20 15:13:22
 myFriends.png, Krakow, 2013-09-05 14:07:13
 Eiffel.jpg, Florianopolis, 2015-07-23 08:03:02
@@ -20,7 +21,7 @@ f.png, Krakow, 2016-01-02 10:55:32
 g.jpg, Krakow, 2016-02-29 22:13:11"
 
 expected =
-"Krakow02.jpg
+  "Krakow02.jpg
 London1.png
 Krakow01.png
 Florianopolis2.jpg
@@ -37,11 +38,10 @@ Krakow05.png
 Krakow10.jpg"
 
 describe ImageSorter do
-    describe '#run' do
-        it "renames a string of listed images based on location and date sorted in the original order provided" do
-            sorter = ImageSorter.new
-            expect(sorter.run(payload)).to eq(expected)
-        end
+  describe '#run' do
+    it 'renames a string of listed images based on location and date sorted in the original order provided' do
+      sorter = ImageSorter.new
+      expect(sorter.run(payload)).to eq(expected)
     end
+  end
 end
-  
